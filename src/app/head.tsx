891239322d832de280542344f40c10f22c27fde3
Daniel Hug <daniel.hug@bluewin.ch>
2025-05-06 19:06:54 +0200

    Version Flavicon

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -5,7 +5,7 @@ export default function Head() {
         <meta name="description" content="Quantum Computing. Demystified." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon-rounded.ico" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon-square.png" />
         <meta name="theme-color" content="#0f172a" />
       </>
     );
