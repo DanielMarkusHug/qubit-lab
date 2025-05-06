58a798843e2bc5e362261160d249b58d756c82d9
Daniel Hug <daniel.hug@bluewin.ch>
2025-05-06 17:02:58 +0200

    Version Flavicon

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
         <title>Qubit Lab</title>
         <meta name="description" content="Quantum Computing. Demystified." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon-rounded.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="theme-color" content="#0f172a" />
       </>
