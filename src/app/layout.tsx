4374a81a71b5f6e13891306eed647f5288b48def
Daniel Hug <daniel.hug@bluewin.ch>
2025-05-08 20:57:38 +0200

    Version Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,11 +13,6 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Qubit Lab",
-  description: "Quantum Computing Demystified",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
