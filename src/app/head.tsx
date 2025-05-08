5c1051b32cf2d64607effa12f51ae95088d74e88
Daniel Hug <daniel.hug@bluewin.ch>
2025-05-08 19:07:21 +0200

    Version Analytics

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -8,8 +8,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         
         {/* Favicon for modern browsers */}
-        <link rel="icon" type="image/x-icon" href="/favicon-v2.ico" />
-        <link rel="shortcut icon" href="/favicon-v2.ico" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" href="/favicon-v2-32.png" sizes="32x32" />
         <link rel="icon" type="image/png" href="/favicon-v2-16.png" sizes="16x16" />
         
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import Link from "next/link";
 import Giscus from '@giscus/react';
 import React from "react";
-
+import { Analytics } from "@vercel/analytics/react"
 
 const quantumResources = [
   {
@@ -230,7 +230,7 @@ export default function Home() {
       <section className="max-w-3xl mx-auto px-6 py-10 relative z-10">
         <div className="rounded-2xl bg-gradient-to-br from-cyan-500 to-blue-500 p-1 shadow-xl hover:scale-[1.01] transition">
           <div className="bg-black rounded-2xl p-6">
-            <h2 className="text-3xl font-bold text-center text-white mb-4">🎬 Let Us Explain...</h2>
+            /*<h2 className="text-3xl font-bold text-center text-white mb-4">🎬 Let Us Explain...</h2>*/
             <div className="relative w-full mb-4 overflow-hidden rounded-xl" style={{ paddingBottom: '56.25%' }}>
               <iframe
                 src="https://www.youtube.com/embed/bIrVSk6OIM0?rel=0"
@@ -241,7 +241,7 @@ export default function Home() {
                 className="absolute top-0 left-0 w-full h-full"
               />
             </div>
-            <p className="text-center text-gray-300 text-xl">A quick overview of what Qubit Lab is all about.</p>
+            <p className="text-center text-gray-300 text-2xl">A quick overview of what Qubit Lab is all about.</p>
           </div>
         </div>
       </section>
