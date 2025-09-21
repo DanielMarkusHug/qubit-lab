3efd55e40436721977067042ab13d4868eb8e7e8
Daniel Hug <daniel.hug@bluewin.ch>
2025-09-21 19:12:14 +0200

    Redesign

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,27 @@
 import "./globals.css";
 import { ReactNode } from "react";
-import BackgroundVideo from "@/components/BackgroundVideo";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="text-white relative min-h-screen">
-        <BackgroundVideo />
-        <div className="relative z-10 min-h-screen">{children}</div>
+      <body className="text-white">
+        <main className="relative min-h-screen">
+          {/* Background */}
+          <video
+            src="/background.mp4"
+            className="fixed inset-0 w-full h-full object-cover scale-125 pointer-events-none z-0 brightness-50"
+            autoPlay
+            muted
+            loop
+            playsInline
+            preload="auto"
+            aria-hidden="true"
+          />
+          <div className="absolute inset-0 bg-black/70 z-0" />
+
+          {/* Content */}
+          <div className="relative z-10">{children}</div>
+        </main>
       </body>
     </html>
   );
