ad3569db74c2db2e6eb12b1d06f7b5b186f24400
Daniel Hug <daniel.hug@bluewin.ch>
2025-09-23 07:24:33 +0200

    Redesign

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Analytics } from "@vercel/analytics/react";
 import { useEffect, useRef } from "react";
 import Image from "next/image";
 import { videos } from "@/data/videos";
